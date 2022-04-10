8bd9940888fef788d26e4981814fc906baa0ffcd
MilesPoupart <renjunyang007@126.com>
2022-04-10 23:36:00 +0800

    fix

--- nju_health_report.py
+++ nju_health_report.py
@@ -178,7 +178,7 @@ HEADERS_LOGIN = {
     "Connection": "keep-alive",
     "Pragma": "no-cache",
     "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36 Edg/100.0.1185.29",
 }
 
 # spider service
